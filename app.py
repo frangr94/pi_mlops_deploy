13c3d2a04d3a81a0fc64af08682fc366245af681
fran <frangr94>
2023-07-07 16:40:12 -0300

    ya fue

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ def get_duracion(Pelicula: str):
     if duracion==0:
         duracion='no-data'
 
-    #respuesta='la pelicula {} dura {} minutos y fue estrenada en {}'.format(nombre,duracion,año)
-    respuesta=[{'title':nombre,'duracion':duracion,'year':año}]
+    respuesta='la pelicula {} dura {} minutos y fue estrenada en {}'.format(nombre,duracion,año)
+    #respuesta=[{'title':nombre,'duracion':duracion,'year':año}]
     
     return respuesta
 
