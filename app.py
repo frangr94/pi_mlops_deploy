402a93b1904a8cd7a2e516fe783f92a3395812d7
fran <frangr94>
2023-07-07 15:27:45 -0300

    bugs api corregidos

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_duracion(Pelicula: str):
         duracion='[no-data]'
 
     #respuesta='la pelicula {} dura {} minutos y fue estrenada en {}'.format(nombre,duracion,año)
-    respuesta=[{'title':Pelicula,'duracion':duracion,'year':año}]
+    respuesta=[{'title':nombre,'duracion':duracion,'year':año}]
     
     return respuesta
 
